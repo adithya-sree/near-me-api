031f3cd5cd6d32b47ead7a789a55034fb6f20446
exerdra <exerdra@gmail.com>
2020-05-30 05:43:46 -0500

    code clean up

--- app/app.go
+++ app/app.go
@@ -9,11 +9,13 @@ import (
 	"github.com/gorilla/mux"
 )
 
+//App app struct
 type App struct {
 	Router  *mux.Router
 	Handler *handler.Handler
 }
 
+//Initialize creates db & initializes connection
 func (a *App) Initialize(connection string) error {
 	db, err := db.NewClient(connection)
 	if err != nil {
@@ -28,7 +30,6 @@ func (a *App) Initialize(connection string) error {
 }
 
 func (a *App) setRoutes() {
-
 	a.Router.HandleFunc("/", a.Handler.Base)
 	a.Router.HandleFunc("/api", a.Handler.Base)
 	a.Router.HandleFunc("/api/uptime", a.Handler.Uptime)
@@ -37,6 +38,7 @@ func (a *App) setRoutes() {
 	a.Router.HandleFunc("/api/location", a.Handler.GetLocation).Methods("GET")
 }
 
+//Run runs the mux server
 func (a *App) Run(addr string) {
 	log.Fatal(http.ListenAndServe(addr, a.Router))
 }
